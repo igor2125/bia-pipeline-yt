e0e6c9682f74c564b8ef9af6e1a5b7dc067b7c2f
Igor Farias <igor2125@gmail.com>
2025-06-29 12:52:48 -0300

    Portugues

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "red" : "blue"}
-          text={showAdd ? "close" : "Add"}
+          text={showAdd ? "Fecha" : "Add"}
           onClick={onAdd}
         />
       )}
