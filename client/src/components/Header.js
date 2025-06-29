8e704b3db3ed4bbeafa286788ca37c4b5eec1e76
Igor Farias <igor2125@gmail.com>
2025-06-29 13:10:01 -0300

    Teste

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "red" : "blue"}
-          text={showAdd ? "Fech" : "Add"}
+          text={showAdd ? "Fecha" : "Add"}
           onClick={onAdd}
         />
       )}
